26c08d6a589e8424258983928ac23486ad95fdc1
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2021-08-19 20:34:20 +0200

    recipe-list

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+const recipes = [
+    {
+        name: 'Spaghetti Carbonara',
+        level: 'medium',
+        ingredients: ['Egg', 'Salt', 'Pepper', 'Pasta', 'Bacon'],
+        preparation: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel at quos molestiae nihil eveniet architecto veritatis unde provident nobis sint?'
+    },
+    {
+        name: 'Pancakes',
+        level: 'easy',
+        ingredients: ['Egg', 'Salt', 'Pepper', 'Pasta', 'Bacon'],
+        preparation: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel at quos molestiae nihil eveniet architecto veritatis unde provident nobis sint?'
+    },
+    {
+        name: 'Roll',
+        level: 'medium',
+        ingredients: ['Egg', 'Salt', 'Pepper', 'Pasta', 'Bacon'],
+        preparation: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel at quos molestiae nihil eveniet architecto veritatis unde provident nobis sint?'
+    },
+    {
+        name: 'Omlette',
+        level: 'easy',
+        ingredients: ['Egg', 'Salt', 'Pepper', 'Pasta', 'Bacon'],
+        preparation: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel at quos molestiae nihil eveniet architecto veritatis unde provident nobis sint?'
+    }
+];
+
+export default recipes;
