cd4b295f5950d4c0eafa10d995008073bcba54eb
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2021-08-30 15:33:19 +0200

    fix

--- src/components/oragnisms/NewRecipeForm/NewRecipeForm.js
+++ src/components/oragnisms/NewRecipeForm/NewRecipeForm.js
@@ -1,14 +1,24 @@
-import { StyledForm, StyledTitle, StyledButton } from './NewRecipeFrom.styled';
+import { StyledForm, StyledTitle, StyledButton, StyledSelect } from './NewRecipeFrom.styled';
+import recipes from 'data/data';
 
 const NewRecipeForm = () => {
+
+
     return ( 
         <>
             <StyledTitle>Add new recipe</StyledTitle>
-            <StyledForm action="">
+            <StyledForm >
+                <label>Title</label>
+                <input type="text" />
                 <label>Ingredients</label>
                 <input type="text" />
                 <label>Preparation</label>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea name="preparation" id="" cols="30" rows="10"></textarea>
+                <StyledSelect name="Level">
+                    <option value="Easy">Easy</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Hard">Hard</option>
+                </StyledSelect>
                 <StyledButton type="submit">Add new recipe</StyledButton>
             </StyledForm>
         </>
